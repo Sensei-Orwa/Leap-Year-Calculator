71b4cfe04a5f860dbd44618385ee8773113ef862
Sensei Orwa <146319656+Sensei-Orwa@users.noreply.github.com>
2023-10-06 18:33:04 +0300

    Add files via upload

---
+++ Leap Year Calculator.py
@@ -0,0 +1,28 @@
+#LEAP YEAR CALCULATOR
+year = int(input("Enter year: "))
+
+
+#ALGORITHM
+if year % 4 == 0:
+
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"The year {year} is a leap year")
+        else:
+            print(f"The year {year} is not a leap year")
+    else:
+        print(f"The year {year} is not a leap year")
+else:
+    print(f"The year {year} is not a leap year")
+
+
+
+
+
+
+
+
+
+
+
+
